22e54a3ceebd63ffe10fe048403647883546bf3d
bear <15223245@qq.com>
2026-08-22 14:11:52 +0800

    fix: adapt message_bridge for Flutter 3 modern embedding and AGP namespace

--- android/src/main/java/auto/whiterhino/module/message_bridge/MessageBridgePlugin.java
+++ android/src/main/java/auto/whiterhino/module/message_bridge/MessageBridgePlugin.java
@@ -38,7 +38,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** MessageBridgePlugin */
 public class MessageBridgePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
